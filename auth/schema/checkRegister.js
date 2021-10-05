e8d77e012da4507ed482760e717f36f17ee71791
mkoys <mkoys@protonmail.com>
2021-10-05 21:14:54 +0200

    Added username and password length checks and settings

--- auth/schema/checkRegister.js
+++ auth/schema/checkRegister.js
@@ -1,3 +1,5 @@
+const settings = require("../../settings.js");
+
 const isObject = require("../../utils/isObject.js");
 
 module.exports = function (data) {
@@ -19,6 +21,16 @@ module.exports = function (data) {
     if (typeof data.username !== "string" || typeof data.email !== "string" || typeof data.password !== "string") {
         result.code = 400;
         return result;
+    }
+
+    if(data.username.length <= settings.usernameLength) {
+        result.code = 400;
+        result.messages.push("Username is too short")
+    }
+
+    if(data.password.length <= settings.passwordLength) {
+        result.code = 400;
+        result.messages.push("Password is too short")
     }
 
     return result;
--- settings.js
+++ settings.js
@@ -1,3 +1,5 @@
 module.exports = {
     port: 8000, // Port that application will be served on
+    usernameLength: 4, // Minimum lenght of username
+    passwordLength: 8, // Minimum lenght of password
 }
