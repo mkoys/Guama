7094eae376e9e7ccdc7d7d998dfc519f532ac47e
mkoys <mkoys@protonmail.com>
2021-10-05 10:01:25 +0200

    First commit!

---
+++ README.md
@@ -0,0 +1,20 @@
+# 🔥️ Guama Server
+## Dependecies
+- **Expresss** --> API framework
+- **Socket.IO** --> Realtime Websocket communication
+- **Cors** --> Set's cross origin resource headers
+- **Helmet** --> Set's some headers to be mroe secure
+- **Morgan** --> Small http logger
+- **Nodemon** --> Development code live realoader
+
+## Work Log 📔️
+- [x] **Create boilerplate code with base dependecies**
+	* [x] Import in modules
+	* [x] Create Instances of Http, Express, Socket
+	* [x] Use express middlewares
+	* [x] Create hello world route
+	* [x] Comment code
+- [x] **Create and push to GitHub repo**
+	* [x] Create repo
+	* [x] Push boilerplate code
+	
---
+++ app.js
@@ -0,0 +1,38 @@
+// Import settings
+const settings = require("./settings.js");
+
+// Import Native HTTP node module
+const http = require("http");
+
+// Import Socket
+const { Server } = require("socket.io");
+
+// Import Express
+const express = require("express");
+
+// Import Express middleware
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
+
+// Create Express, HTTP and Socket server instances
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Use express middleware 
+app.use(express.json()); // JSON parser
+app.use(helmet()); // Set's some headers to be more secure
+app.use(cors()); // Set's Cross Origin Access Headers
+app.use(morgan("tiny")); // Small HTTP logger
+
+// GET Route hello world
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+// Constant for port
+const port = settings.port || 8000;
+
+// Server listen on port and log message
+server.listen(port, () => {console.log(`Running on http://localhost:${port}`)});
